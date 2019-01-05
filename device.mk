a7662a7c6e7729586679969ab9d652a99168dc95
Hamster Tian <haotia@gmail.com>
2019-01-05 23:52:18 +0800

    dragon: Use grid style recents view
    Tablets are well suited for this.

--- device.mk
+++ device.mk
@@ -230,7 +230,8 @@ $(call inherit-product, frameworks/native/build/tablet-10in-xhdpi-2048-dalvik-he
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.adb.secure=1 \
     ro.sf.lcd_density=320 \
-    ro.opengles.version=196610
+    ro.opengles.version=196610 \
+    ro.recents.grid=true
 
 # for audio
 #TODO(dgreid) do we need libnvvisualizer?
