6045b2d0164fedc0311d2980d7696132e6d79947
1582130940 <1582130940@qq.com>
2019-07-08 00:01:57 +0800

    dragon: Set ro.hwui.text cache to 2048

--- device.mk
+++ device.mk
@@ -231,10 +231,10 @@ PRODUCT_PROPERTY_OVERRIDES := \
     ro.hwui.gradient_cache_size=1 \
     ro.hwui.drop_shadow_cache_size=6 \
     ro.hwui.texture_cache_flushrate=0.4 \
-    ro.hwui.text_small_cache_width=1024 \
-    ro.hwui.text_small_cache_height=1024 \
+    ro.hwui.text_small_cache_width=2048 \
+    ro.hwui.text_small_cache_height=2048 \
     ro.hwui.text_large_cache_width=2048 \
-    ro.hwui.text_large_cache_height=1024 \
+    ro.hwui.text_large_cache_height=2048 \
     ro.hwui.disable_scissor_opt=true
 
 # The default locale should be determined from VPD, not from build.prop.
