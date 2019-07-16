07748ebb9f67fecb148bd0647d6a8253d39f0b2f
1582130940 <1582130940@qq.com>
2019-07-16 16:55:55 +0800

    dragon: Add build fingerprint from stock rom

--- lineage_dragon.mk
+++ lineage_dragon.mk
@@ -36,3 +36,10 @@ PRODUCT_MANUFACTURER := Google
 PRODUCT_RESTRICT_VENDOR_FILES := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-google
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME="dragon" \
+    PRIVATE_BUILD_DESC="ryu-user 8.1.0 OPM8.190605.003 5524042 release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := google/ryu/dragon:8.1.0/OPM8.190605.003/5524042:user/release-keys
